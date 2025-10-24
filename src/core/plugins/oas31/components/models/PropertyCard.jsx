6e80652f8cfec8f8fd35e2352617e071cbd6bcde
a.radzhabov <afrosrajabov1207@mail.ru>
2025-10-24 17:56:54 +0700

    Fixed property card [added properties list]

---
+++ src/core/plugins/oas31/components/models/PropertyCard.jsx
@@ -0,0 +1,79 @@
+/**
+ * @prettier
+ */
+import React from "react"
+import PropTypes from "prop-types"
+
+const refPrefix = "#/components/schemas/"
+
+const PropertyCard = ({ 
+  property, 
+  index, 
+  onRemove, 
+  safeExtractSchemaName 
+}) => {
+  const renderPropertyInfo = () => {
+    if (property.anyOf || property.oneOf || property.allOf) {
+      const compositionType = property.anyOf ? 'anyOf' : property.oneOf ? 'oneOf' : 'allOf'
+      const schemas = property[compositionType].map(ref => 
+        safeExtractSchemaName(ref.$ref || ref.$$ref)
+      ).join(', ')
+      return `(${compositionType}: ${schemas}${property.required ? ', required' : ''})`
+    } else {
+      let typeDisplay = property.type.includes(refPrefix) 
+        ? safeExtractSchemaName(property.type) 
+        : property.type
+      
+      // Handle array types to show items type
+      if (property.type === 'array' && property.itemsType) {
+        const itemsType = property.itemsType.includes(refPrefix)
+          ? safeExtractSchemaName(property.itemsType)
+          : property.itemsType
+        typeDisplay = `array[${itemsType}]`
+      }
+      
+      return `(${typeDisplay}${property.format ? `, ${property.format}` : ''}${property.required ? ', required' : ''})`
+    }
+  }
+
+  return (
+    <div className="property-card" style={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: '10px',
+      margin: '5px 0',
+      border: '1px solid #e0e0e0',
+      borderRadius: '4px',
+      backgroundColor: '#f9f9f9'
+    }}>
+      <div className="property-info">
+        <strong>{property.name}</strong>
+        <span style={{ margin: '0 10px', color: '#666' }}>
+          {renderPropertyInfo()}
+        </span>
+        {property.description && (
+          <span style={{ color: '#666', fontSize: '0.9em' }}>
+            - {property.description}
+          </span>
+        )}
+      </div>
+      <button 
+        type="button" 
+        className="btn btn-danger btn-sm" 
+        onClick={() => onRemove(index)}
+      >
+        Remove
+      </button>
+    </div>
+  )
+}
+
+PropertyCard.propTypes = {
+  property: PropTypes.object.isRequired,
+  index: PropTypes.number.isRequired,
+  onRemove: PropTypes.func.isRequired,
+  safeExtractSchemaName: PropTypes.func.isRequired
+}
+
+export default PropertyCard
